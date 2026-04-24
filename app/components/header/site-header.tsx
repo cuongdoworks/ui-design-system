31e7e5e8fabb9ebba4824bce654ace19d3f882aa
cuongdo <cuong.do.phi@247express.vn>
2026-04-24 10:34:17 +0700

    feat: implement application sidebar, site header, and theme switching providers

---
+++ app/components/header/site-header.tsx
@@ -0,0 +1,42 @@
+import { Separator } from '@/components/ui/separator';
+import { SidebarTrigger } from '@/components/ui/sidebar';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+
+import { ModeToggle } from '@/components/mode-toggle';
+
+export function SiteHeader() {
+  return (
+    <header className='flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 border-b'>
+      <div className='flex items-center gap-2 px-4 w-full'>
+        <div className='flex items-center gap-2'>
+          <SidebarTrigger className='-ml-1' />
+          <Separator
+            orientation='vertical'
+            className='mr-2 data-[orientation=vertical]:h-4'
+          />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className='hidden md:block'>
+                <BreadcrumbLink href='#'>Build Your Application</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className='hidden md:block' />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+        <div className='ml-auto'>
+          <ModeToggle />
+        </div>
+      </div>
+    </header>
+  );
+}
